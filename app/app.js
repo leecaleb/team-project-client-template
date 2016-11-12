2e4b04567de9ac1eb15a6424130bb79b6e0e9231
Mi Li <mili@umass.edu>
2016-11-12 11:32:35 -0500

    add navbar

--- app/app.js
+++ app/app.js
@@ -5,6 +5,39 @@ import FavoriteFeed from './components/favoriteFeed';
 //import Feed from './components/feed';
 //import { IndexRoute, Router, Route, browserHistory } from 'react-router'
 
+class Navbar extends React.Component {
+  render() {
+    return (
+      <div className="container-fluid">
+        <div className="navbar-header">
+          <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+            <span className="sr-only">Toggle navigation</span>
+            <span className="icon-bar"></span>
+            <span className="icon-bar"></span>
+            <span className="icon-bar"></span>
+          </button>
+          <a className="navbar-brand" href="#">WebApp</a>
+        </div>
+
+        <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+          <ul className="nav navbar-nav navbar-right">
+                <li className="dropdown pull-right">
+                <a href="#" className="dropdown-toggle" data-toggle="dropdown" role="button">
+                  <span className="glyphicon glyphicon-menu-hamburger burger-menu"></span>
+                </a>
+                <ul className="dropdown-menu">
+                  <li><a href="#">Edit profile</a></li>
+                  <li role="separator" className="divider"></li>
+                  <li><a href="#">Sign out</a></li>
+                </ul>
+              </li>
+          </ul>
+        </div>
+      </div>
+    );
+  }
+}
+
 class LeftSidebar extends React.Component {
   render() {
     return (
@@ -159,6 +192,11 @@ class Search extends React.Component {
     )
   }
 }
+
+ReactDOM.render(
+  <Navbar />,
+  document.getElementById('navbar')
+);
 
 ReactDOM.render(
   <div>
