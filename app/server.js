e27d7ad8fc8dcd33fc479455ec308ca7ccfc1adc
Chia-Lei Lee <chialeilee@umass.edu>
2016-11-16 17:58:43 -0500

    progress1

--- app/server.js
+++ app/server.js
@@ -16,15 +16,8 @@ function emulateServerReturn(data, cb) {
 */
 function getFeedItemSync(feedItemId) {
   var feedItem = readDocument('feedItems', feedItemId);
-  // Resolve 'like' counter.
-  feedItem.likeCounter =
-  feedItem.likeCounter.map((id) => readDocument('users', id));
-  // Assuming a StatusUpdate. If we had other types of
-  // FeedItems in the DB, we would
-  // need to check the type and have logic for each type.
   feedItem.contents.author =
-  readDocument('users', feedItem.contents.author);
-  // Resolve comment author.
+    readDocument('users', feedItem.contents.author);
   feedItem.comments.forEach((comment) => {
     comment.author = readDocument('users', comment.author);
   });
@@ -49,6 +42,17 @@ export function getFeedData(user, cb) {
   // emulateServerReturn will emulate an asynchronous server operation, which
   // invokes (calls) the "cb" function some time in the future.
   emulateServerReturn(feedData, cb);
+}
+
+export function postComment(spotId, user, contents, cb) {
+  var spot = readDocument('feedItems', spotId);
+  spot.comments.push({
+      "author": user,
+      "postDate": new Date().getTime(),
+      "contents": contents
+    });
+  writeDocument('feedItems', spot);
+  emulateServerReturn(getFeedItemSync(spotId), cb);
 }
 
 
