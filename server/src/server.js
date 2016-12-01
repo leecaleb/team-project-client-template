fd4fce307972b344d593498f31ad74dca4c7e6c7
Mi Li <mili@umass.edu>
2016-12-01 16:45:05 -0500

    hello world + initialData on server

--- server/src/server.js
+++ server/src/server.js
@@ -1,2 +1,17 @@
 // Implement your server in this file.
 // We should be able to run your server with node src/server.js
+
+// create express
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+app.use(express.static('../client/build'));
+app.use(bodyParser.text());
+
+app.get('/', function (req, res) { res.send('Hello World!');
+});
+
+app.listen(3000, function () {
+console.log('Example app listening on port 3000!');
+});
