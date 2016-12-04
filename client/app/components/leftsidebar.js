526b4586faa72df04e6688855a46502aeae90514
Mi Li <mili@umass.edu>
2016-12-03 22:38:49 -0500

    delete user rating

--- client/app/components/leftsidebar.js
+++ client/app/components/leftsidebar.js
@@ -33,28 +33,6 @@ export default class LeftSidebar extends React.Component {
                   <td><span className="glyphicon glyphicon-user"></span> Join Since </td>
                   <td>{this.state.joinDate}</td>
                 </tr>
-                <tr>
-                  <td><span className="glyphicon glyphicon-signal"></span> User Rating </td>
-                  <td>
-                    <form className="rating">
-                      <input type="radio" className="rating-input"
-                        id="rating-input-1-5" name="user-rating" />
-                      <label htmlFor="rating-input-1-5" className="rating-star"></label>
-                      <input type="radio" className="rating-input"
-                        id="rating-input-1-4" name="user-rating" />
-                      <label htmlFor="rating-input-1-4" className="rating-star"></label>
-                      <input type="radio" className="rating-input"
-                        id="rating-input-1-3" name="user-rating" />
-                      <label htmlFor="rating-input-1-3" className="rating-star"></label>
-                      <input type="radio" className="rating-input"
-                        id="rating-input-1-2" name="user-rating" />
-                      <label htmlFor="rating-input-1-2" className="rating-star"></label>
-                      <input type="radio" className="rating-input"
-                        id="rating-input-1-1" name="user-rating" />
-                      <label htmlFor="rating-input-1-1" className="rating-star"></label>
-                    </form>
-                  </td>
-                </tr>
               </tbody>
             </table>
           </div>
