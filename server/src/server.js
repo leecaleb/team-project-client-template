dfed342c1878712aa6f5402ef94e996eb8923f32
Mi Li <mili@umass.edu>
2016-12-15 20:44:34 -0500

    cleanup server.js

--- server/src/server.js
+++ server/src/server.js
@@ -1,71 +1,42 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
-
 var ResetDatabase = require('./resetdatabase.js');
-
-
 var CommentSchema = require('./schemas/comment.json');
 var validate = require('express-jsonschema').validate;
-
 var mongo_express = require('mongo-express/lib/middleware');
 var mongo_express_config = require('mongo-express/config.default.js');
-
 var MongoDB = require('mongodb');
 var MongoClient = MongoDB.MongoClient;
 var ObjectID = MongoDB.ObjectID;
 var url = 'mongodb://localhost:27017/Jujube';
 
 MongoClient.connect(url, function(err, db) {
-
-
-
-  // Put everything that uses `app` into this callback function.
-  // from app.use(bodyParser.text());
-  // all the way to
-  // app.listen(3000, ...
   app.use(bodyParser.text());
   app.use(bodyParser.json());
   app.use('/mongo_express', mongo_express(mongo_express_config));
   app.use(express.static('../client/build'));
 
-
-
-
-
   app.get('/feed/:spotid', function(req, res) {
     var spotid = req.params.spotid;
-
     // var fromUser = getUserIdFromToken(req.get('Authorization'));
     // var useridNumber = parseInt(userid, 10);
 
     // if (fromUser === useridNumber) {
     getFeedData(new ObjectID(spotid), function(err, feedData) {
-   if (err) {
-     // A database error happened.
-     // Internal Error: 500.
-
-     res.status(500).send("Database error: " + err);
-
-   } else if (feedData === null) {
-     // Couldn't find the feed in the database.
-
-
-     res.status(400).send("Could not look up feed for spot " + spotid);
-   } else {
-     // Send data.
-
-     res.send(feedData);
-   }
- });
-
-
+      if (err) {
+        // A database error happened.
+        // Internal Error: 500.
+        res.status(500).send("Database error: " + err);
+      } else if (feedData === null) {
+        // Couldn't find the feed in the database.
+        res.status(400).send("Could not look up feed for spot " + spotid);
+      } else {
+        // Send data.
+        res.send(feedData);
+      }
+    });
   });
-
-
-
-
-
 
   app.get('/user/:userid/feed', function(req, res) {
     var userid = req.params.userid;
@@ -79,23 +50,22 @@ MongoClient.connect(url, function(err, db) {
     }
   });
 
-
   //POST
   app.post('/comment', function(req, res) {
-      var body = req.body;
-      var userId = body.userId;
-      var spotId = body.spotId;
-      var contents = body.contents;
-      var rating = body.rating;
-      // var fromUser = getUserIdFromToken(req.get('Authorization'));
-      // if(fromUser === userId) {
-      var newComment = postComment(userId, spotId, contents, rating);
-        res.status(201);
-        // res.set('Location', '/feeditem/' + newComment._id);
-        res.send(newComment);
-      // } else {
-      //   res.status(401).end();
-      // }
+    var body = req.body;
+    var userId = body.userId;
+    var spotId = body.spotId;
+    var contents = body.contents;
+    var rating = body.rating;
+    // var fromUser = getUserIdFromToken(req.get('Authorization'));
+    // if(fromUser === userId) {
+    var newComment = postComment(userId, spotId, contents, rating);
+    res.status(201);
+    // res.set('Location', '/feeditem/' + newComment._id);
+    res.send(newComment);
+    // } else {
+    //   res.status(401).end();
+    // }
   });
 
   // Reset the database.
@@ -105,9 +75,6 @@ MongoClient.connect(url, function(err, db) {
       res.send();
     });
   });
-
-
-
 
   // GET favFeedData
   app.get('/user/:userid/favfeed', function(req, res) {
@@ -121,70 +88,24 @@ MongoClient.connect(url, function(err, db) {
     }
   });
 
-
-
-
-
-
   app.get('/spot/:spotid', function(req, res) {
     var spotid = req.params.spotid;
     // var fromUser = getUserIdFromToken(req.get('Authorization'));
     // var useridNumber = parseInt(userid, 10);
     getSpotData(new ObjectID(spotid), function(err, spotData) {
-         if (err) {
-           // A database error happened.
-           // Internal Error: 500.
-           res.status(500).send("Database error: " + err);
-         } else if (spotData === null) {
-           // Couldn't find the feed in the database.
-           res.status(400).send("Could not look up info for spot " + spotid);
-         } else {
-           // Send data.
-           res.send(spotData);
-         }
-       });
-
-   });
-
-
-
-  // Also put all of the helper functions that use mock database
-  // methods like readDocument, writeDocument, ...
-
-  function postComment(user, spotId, contents, rating) {
-    var spot = readDocument('feedItems', spotId);
-    var currentScore = spot.contents.latest_score;
-    var score = parseInt(currentScore, 10);
-    var myScore = parseInt(rating, 10);
-    var newScore = (score + myScore) / 2;
-    var strScore = newScore + '';
-    spot.contents.latest_score = strScore;
-    spot.comments.push({
-        "author": user,
-        "postDate": new Date().getTime(),
-        "contents": contents,
-        "rating": rating
-      });
-
-    writeDocument('feedItems', spot);
-    return spot;
-  }
-
-
-  function getFeedItemSync(feedItemId) {
-    var feedItem = readDocument('feedItems', feedItemId);
-    feedItem.contents.author =
-      readDocument('users', feedItem.contents.author);
-    feedItem.comments.forEach((comment) => {
-      comment.author = readDocument('users', comment.author);
+      if (err) {
+        // A database error happened.
+        // Internal Error: 500.
+        res.status(500).send("Database error: " + err);
+      } else if (spotData === null) {
+        // Couldn't find the feed in the database.
+        res.status(400).send("Could not look up info for spot " + spotid);
+      } else {
+        // Send data.
+        res.send(spotData);
+      }
     });
-
-
-    return feedItem;
-  }
-
-
-
+  });
 
   app.get('/user/:userid', function(req, res) {
     var userid = req.params.userid;
@@ -204,14 +125,11 @@ MongoClient.connect(url, function(err, db) {
     }
   });
 
-
-
   app.put('/fave/:userid/:spotid/', function(req, res) {
     var fromUser = getUserIdFromToken(req.get('Authorization'));
     // Convert params from string to number.
     var spotid = parseInt(req.params.spotid, 10);
     var userid = parseInt(req.params.userid, 10);
-
 
     if (fromUser === userid) {
       var userData = readDocument('users', userid);
@@ -227,7 +145,6 @@ MongoClient.connect(url, function(err, db) {
       // 401: Unauthorized.
       res.status(401).end();
     }
-
   });
 
   app.delete('/unfave/:userid/:spotid', function(req, res) {
@@ -253,12 +170,6 @@ MongoClient.connect(url, function(err, db) {
     }
   });
 
-
-
-
-
-
-
   app.use(function(err, req, res, next) {
     if (err.name === 'JsonSchemaValidation') {
       res.status(400).end();
@@ -278,8 +189,6 @@ MongoClient.connect(url, function(err, db) {
       res.status(401).end();
     }
   });
-
-
 
   function resolveUserObjects(userList, callback) {
     // Special case: userList is empty.
@@ -309,8 +218,35 @@ MongoClient.connect(url, function(err, db) {
     }
   }
 
+  function postComment(user, spotId, contents, rating) {
+    var spot = readDocument('feedItems', spotId);
+    var currentScore = spot.contents.latest_score;
+    var score = parseInt(currentScore, 10);
+    var myScore = parseInt(rating, 10);
+    var newScore = (score + myScore) / 2;
+    var strScore = newScore + '';
+    spot.contents.latest_score = strScore;
+    spot.comments.push({
+      "author": user,
+      "postDate": new Date().getTime(),
+      "contents": contents,
+      "rating": rating
+    });
 
+    writeDocument('feedItems', spot);
+    return spot;
+  }
 
+  function getFeedItemSync(feedItemId) {
+    var feedItem = readDocument('feedItems', feedItemId);
+    feedItem.contents.author =
+    readDocument('users', feedItem.contents.author);
+    feedItem.comments.forEach((comment) => {
+      comment.author = readDocument('users', comment.author);
+    });
+
+    return feedItem;
+  }
 
   // function getFeedData(user) {
   //   var userData = readDocument('users', user);
@@ -320,45 +256,40 @@ MongoClient.connect(url, function(err, db) {
   //   return (feedData);
   // }
 
-
   function getSpotData(spot, callback) {
     db.collection('spots').findOne({
-   _id: spot
- }, function(err, spotData) {
-   if (err) {
-     return callback(err);
-   } else if (spotData === null) {
-     // User not found.
-     return callback(null, null);
-   }else{
-     callback(null, spotData);
-   }
+      _id: spot
+    }, function(err, spotData) {
+      if (err) {
+        return callback(err);
+      } else if (spotData === null) {
+        // User not found.
+        return callback(null, null);
+      }else{
+        callback(null, spotData);
+      }
 
- });
- }
+    });
+  }
 
   function getFeedData(spot, callback) {
 
     db.collection('feedItems').findOne({
-   _id: spot
- }, function(err, feedData) {
-   if (err) {
-     return callback(err);
-   } else if (feedData === null) {
-     // User not found.
+      _id: spot
+    }, function(err, feedData) {
+      if (err) {
+        return callback(err);
+      } else if (feedData === null) {
+        // User not found.
 
-     return callback(null, null);
-   }
-   callback(null, feedData);
-
- });
-
-
+        return callback(null, null);
+      }
+      callback(null, feedData);
+    });
     // var spotData = readDocument('feedItems', spot);
 
     // return(feedData);
   }
-
 
   function getUserData(userId, callback) {
     // var userData = readDocument('users', user);
@@ -373,13 +304,9 @@ MongoClient.connect(url, function(err, db) {
       } else if (userData === null) {
         return callback(null, null);
       }
-
       callback(null, userData);
     });
   }
-
-
-
 
   function getFavFeedItemSync(favFeedItemId) {
     var feedItem = readDocument('favFeedItems', favFeedItemId);
@@ -403,7 +330,6 @@ MongoClient.connect(url, function(err, db) {
     var feedData = readDocument('favFeeds', userData.favFeeds);
     return feedData;
   }
-
 
   function getUserIdFromToken(authorizationLine) {
     try {
