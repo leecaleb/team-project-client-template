bc1cc7ea29cc87fa1f41472598cef477585b7815
Andrew Murchison <amurchison@umass.edu>
2016-12-16 22:09:55 -0500

    changed tops spots to places to try

--- client/app/components/rightsidebar.js
+++ client/app/components/rightsidebar.js
@@ -1,38 +1,28 @@
 import React from 'react';
 import RightPost from './rightposts';
-import {getFeedData} from '../server';
+
 
 export default class RightSidebar extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      spot1: "",
-      spot2: "",
-      spot3: "",
-      spot4: "",
-      spot5: "",
-      spot6: ""
 
-    }
-
-  getFeedData("000000000000000000000001", (feedData) => {this.setState({spot1: feedData.contents.latest_score})});
-  // getFeedData("000000000000000000000002", (feedData) => {this.setState({spot2: feedData.contents.latest_score})});
-  // getFeedData("000000000000000000000003", (feedData) => {this.setState({spot3: feedData.contents.latest_score})});
-  // getFeedData("000000000000000000000004", (feedData) => {this.setState({spot4: feedData.contents.latest_score})});
-  // getFeedData("000000000000000000000005", (feedData) => {this.setState({spot5: feedData.contents.latest_score})});
-  // getFeedData("000000000000000000000006", (feedData) => {this.setState({spot6: feedData.contents.latest_score})});
-
-}
 render() {
-var arr = [
-   ["000000000000000000000001", this.state.spot1],
-   ["000000000000000000000002", this.state.spot2],
-   ["000000000000000000000003", this.state.spot3],
-   ["000000000000000000000004", this.state.spot4],
-   ["000000000000000000000005", this.state.spot5],
-   ["000000000000000000000006", this.state.spot6]
-];
+
+  var arr = ["000000000000000000000001",
+   "000000000000000000000002",
+   "000000000000000000000003",
+   "000000000000000000000007",
+   "000000000000000000000005",
+   "000000000000000000000006"];
 //var newarr = arr.sort(function(a,b){return b[1]-a[1]}).reverse();
+
+var num = Math.floor((Math.random() * 5) + 0);
+var id1 = arr[num];
+arr.splice(num, 1);
+num = Math.floor((Math.random() * 4) + 0);
+var id2 = arr[num];
+arr.splice(num, 1);
+num = Math.floor((Math.random() * 3) + 0);
+var id3 = arr[num];
+
 
 
 
@@ -50,15 +40,10 @@ var arr = [
               Spots to Try
             </div>
             </div>
-
-            <RightPost spot= {arr[0][0]} />
-            <RightPost spot= {arr[2][0]} />
-            <RightPost spot= {arr[5][0]} />
-
-
-
-
-            </div>
+               <RightPost spot= {id1} />
+               <RightPost spot= {id2} />
+               <RightPost spot= {id3} />
+             </div>
             </div>
           </div>
         </div>
